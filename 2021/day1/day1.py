ce296e016e4973738bbe9d95a670aba1abbe7dc9
Kyle Johnson <johnson90512@gmail.com>
2021-12-11 20:06:27 -0500

    day1 - complete

---
+++ 2021/day1/day1.py
@@ -0,0 +1,21 @@
+#!/bin/env python3 
+ 
+'''
+The first order of business is to figure out how quickly the depth increases, 
+just so you know what you're dealing with - you never know if the keys will get
+carried into deeper water by an ocean current or a fish or something.
+'''
+
+import sys
+
+count = 0
+x = 0
+
+with open('./2021/day1/input.txt') as f:
+    for line in f.readlines():
+        line_num = int(line)
+        if line_num > x:
+            count = count + 1
+        
+        x = line_num
+    print(count - 1)
