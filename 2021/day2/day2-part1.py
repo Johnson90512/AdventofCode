6407b4514bff8555429348e71cd62c5ed39e7ea1
Kyle Johnson <johnson90512@gmail.com>
2021-12-15 07:42:05 -0500

    started day 2

--- 2021/day2/day2-part1.py
+++ 2021/day2/day2-part1.py
@@ -4,11 +4,20 @@ you would have after following the planned course.
 What do you get if you multiply your final horizontal position by your final depth?
 '''
 cleaned_list = []
+split_list = []
+count = 0
 
 with open('./2021/day2/input.txt') as f:
     lines = f.readlines()
 
 for x in lines:
     cleaned_list.append(x.replace("\n", ""))
-print(cleaned_list)
+
+for i in cleaned_list:
+    split_list.extend(i.split(" "))
+
+for item in split_list:
+    if count % 2 == 0:
+        
+        count = count + 1
 
