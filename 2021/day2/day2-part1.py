920426cd59e65170c157bfe9fe748918f102121d
Kyle Johnson <johnson90512@gmail.com>
2021-12-17 07:48:21 -0500

    Started day 2 part 2

--- 2021/day2/day2-part1.py
+++ 2021/day2/day2-part1.py
@@ -3,9 +3,11 @@ Calculate the horizontal position and depth
 you would have after following the planned course. 
 What do you get if you multiply your final horizontal position by your final depth?
 '''
+
 cleaned_list = []
 split_list = []
-count = 0
+horizontal = 0
+vertical = 0
 
 with open('./2021/day2/input.txt') as f:
     lines = f.readlines()
@@ -14,10 +16,12 @@ for x in lines:
     cleaned_list.append(x.replace("\n", ""))
 
 for i in cleaned_list:
-    split_list.extend(i.split(" "))
-
-for item in split_list:
-    if count % 2 == 0:
-        
-        count = count + 1
-
+    split_list = i.split(" ")
+    if 'forward' in split_list[0]:
+        horizontal += int(split_list[1])
+    if 'up' in split_list[0]:
+        vertical -= int(split_list[1])
+    elif 'down' in split_list[0]:
+       vertical += int(split_list[1])
+       
+print(horizontal * vertical)
